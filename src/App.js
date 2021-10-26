c0a6297157802ce63fb55fe5a8bb4488db456183
Velocityofpie <joshuamuq@gmail.com>
2021-10-25 21:49:59 -0400

    start

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App= ()=> {
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="7eb43a1c-c5af-4acf-9d1a-8d73e155d017"
+            userName="Admin"
+            userSecret="admin"
+        />
+    )
+}
+
+export default App;
