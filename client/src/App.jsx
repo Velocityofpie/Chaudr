abadc6dbef67fc499762504c6c19d3963c89f649
Velocityofpie <joshuamuq@gmail.com>
2021-11-05 16:35:02 -0400

    Huge changes (look here if there is any probs)

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./App.css"
+import Sidebar from './components/Sidebar'
+
+const App = () => {
+    return (
+        <div className="app">
+            <h2>test</h2>
+            {/*Sidebar*/}
+                <Sidebar/>
+            {/*Sidebar*/}
+      </div>
+        
+    )
+}
+
+export default App
